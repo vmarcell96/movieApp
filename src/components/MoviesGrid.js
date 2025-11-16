23de15540a00478cdaeddcb0c6cd671623034d22
vmarcell96 <vajda96@gmail.com>
2025-11-16 18:18:54 +0100

    Add search bar ,search bar functionality

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -4,6 +4,7 @@ import MovieCard from "./MovieCard";
 
 export default function MoviesGrid() {
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
     useEffect(() => {
         fetch("movies.json")
@@ -12,11 +13,29 @@ export default function MoviesGrid() {
         setMovies(movies);
     }, []);
 
+    const handleSearchTermChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const filteredMovies = movies.filter((movie) =>
+        movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
-        <div className="movies-grid">
-            {movies.map((movie) => (
-                <MovieCard movie={movie} key={movie.id} />
-            ))}
+        <div>
+            <input
+                className="search-input"
+                type="text"
+                placeholder="Search movies..."
+                value={searchTerm}
+                onChange={handleSearchTermChange}
+            />
+
+            <div className="movies-grid">
+                {filteredMovies.map((movie) => (
+                    <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div>
         </div>
     );
 }
