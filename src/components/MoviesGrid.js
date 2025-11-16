a245ec57ef9c0447ad520ff24f705ddc3544f356
vmarcell96 <vajda96@gmail.com>
2025-11-16 17:55:09 +0100

    Add MovieCard component

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "../styles.css";
+import MovieCard from "./MovieCard";
 
 export default function MoviesGrid() {
     const [movies, setMovies] = useState([]);
@@ -14,14 +15,7 @@ export default function MoviesGrid() {
     return (
         <div className="movies-grid">
             {movies.map((movie) => (
-                <div key={movie.id} className="movie-card">
-                    <img src={`images/${movie.image}`} alt={movie.title} />
-                    <div className="movie-card-info">
-                        <h3 className="movie-card-title">{movie.title}</h3>
-                        <p className="movie-card-genre">{movie.genre}</p>
-                        <p className="movie-card-rating">{movie.rating}</p>
-                    </div>
-                </div>
+                <MovieCard movie={movie} />
             ))}
         </div>
     );
